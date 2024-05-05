8ee0f60945ef2997cacca0ed765a172426ef7107
Saira Batool <sairabatool787@gmail.com>
2024-05-04 22:03:50 -0700

    Completed

---
+++ Multiplication/mult.js
@@ -0,0 +1,14 @@
+alert("Hi! Here You Can Multiply Two Numbers")
+
+
+var num1 = prompt("Enter a First Number")
+var num2 = prompt("Enter a First Number")
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+var result = num1 * num2
+
+alert("After Product : " + result)
+
+alert("Hope You Like it 😊");
---
+++ Subraction/sub.js
@@ -0,0 +1,14 @@
+alert("Hi! Here You Can Subtract Two Numbers")
+
+
+var num1 = prompt("Enter a First Number")
+var num2 = prompt("Enter a Second Number")
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+var result = num1 - num2;
+
+alert("After Subtracted : " + result)
+
+alert("Hope You Like it 😊");
